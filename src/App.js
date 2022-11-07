3e61bbb4d3f56a7d090557c6ac0a5c8faad4aa6e
arifur <abirarifur@gmail.com>
2022-11-07 17:50:50 +0600

    setting routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,37 @@
-// import Home from "./pages/Home";
+import Cart from "./pages/Cart";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Product from "./pages/Product";
 import ProductList from "./pages/ProductList";
-// import Product from "./pages/Product";
-// import Login from "./pages/Login";
-// import Register from "./pages/Register";
-// import Cart from "./pages/Cart";
+import Register from "./pages/Register";
+
+import {
+  BrowserRouter as Router,
+  Navigate,
+  Route,
+  Routes,
+} from "react-router-dom";
 
 function App() {
+  const user = true;
   return (
-    <div>
-      <ProductList />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products/:category" element={<ProductList />} />
+        <Route path="/product/:id" element={<Product />} />
+        <Route path="/cart" element={<Cart />} />
+
+        <Route
+          path="/login"
+          element={user ? <Navigate to="/" replace={true} /> : <Login />}
+        />
+        <Route
+          path="/register"
+          element={user ? <Navigate to="/" replace={true} /> : <Register />}
+        />
+      </Routes>
+    </Router>
   );
 }
 
