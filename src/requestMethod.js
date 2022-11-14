2861e6d5119d4c03ae7f9437e93d38e0195fbebc
arifur <abirarifur@gmail.com>
2022-11-14 16:57:50 +0600

    final commit

--- src/requestMethod.js
+++ src/requestMethod.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 const BASE_URL = "http://localhost:5000/api/";
 
-const TOKEN =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzNjM4ZWMzZmE1N2EzNWNmMTdhZTIyZiIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY2ODI0ODEyMywiZXhwIjoxNjY4NTA3MzIzfQ.4-IE117O_lCPtrvYZ3wsXrC7OCjZ4eHOV7MCOSLKOw4";
+const TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user)
+  .currentUser?.accessToken;
 
 export const publicRequest = axios.create({
   baseURL: BASE_URL,
